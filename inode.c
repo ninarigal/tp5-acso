dca9d8daf7b4f77db0f6ad078875f48e581d7c36
Nina Rigal <ninarigal@MacBook-Pro-de-Nina.local>
2024-06-04 20:54:04 -0300

    inode

--- inode.c
+++ inode.c
@@ -122,52 +122,41 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
 // }
 
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
-    int fd = fs->dfd;
-    int is_small_file = ((inp->i_mode & ILARG) == 0);
+	int fd = fs->dfd;
+	int is_small_file = ((inp->i_mode & ILARG) == 0);
 
-    // if it is a small file
-    if (is_small_file) {
-        if (blockNum < 7) {
-            return inp->i_addr[blockNum];
-        } else {
-            return -1; // Block number out of range for small files
-        }
-    }
+	// if it is a small file
+	if(is_small_file) {
+		return inp->i_addr[blockNum];
+	}	
 
-    // if it is a large file
-    int addr_num = DISKIMG_SECTOR_SIZE / sizeof(uint16_t);
-    int indir_addr_num = addr_num * 7;
+	// if it is a large file
+	int addr_num = DISKIMG_SECTOR_SIZE / sizeof(uint16_t);
+	int indir_addr_num = addr_num * 7;
+	if(blockNum < indir_addr_num) {		// if it only uses INDIR_ADDR
+		int sector_offset = blockNum / addr_num;
+		int addr_offset = blockNum % addr_num;
+		uint16_t addrs[addr_num];
+		int err = diskimg_readsector(fd, inp->i_addr[sector_offset], addrs);
+		if(err < 0) return -1;	
+		return addrs[addr_offset];
+	} else {							// if it also uses the DOUBLE_INDIR_ADDR
+		// the first layer
+		int blockNum_in_double = blockNum - indir_addr_num;
+		int sector_offset_1 = 7;
+		int addr_offset_1 = blockNum_in_double / addr_num;
+		uint16_t addrs_1[addr_num];
+		int err_1 = diskimg_readsector(fd, inp->i_addr[sector_offset_1], addrs_1);
+		if(err_1 < 0) return -1;
 
-    if (blockNum < indir_addr_num) {  // if it only uses single indirect addressing
-        int sector_offset = blockNum / addr_num;
-        int addr_offset = blockNum % addr_num;
-        uint16_t addrs[addr_num];
-        
-        if (diskimg_readsector(fd, inp->i_addr[sector_offset], addrs) < 0) {
-            return -1;
-        }
-        return addrs[addr_offset];
-    } else {  // if it also uses double indirect addressing
-        // the first layer
-        int blockNum_in_double = blockNum - indir_addr_num;
-        int sector_offset_1 = 7;
-        int addr_offset_1 = blockNum_in_double / addr_num;
-        uint16_t addrs_1[addr_num];
-        
-        if (diskimg_readsector(fd, inp->i_addr[sector_offset_1], addrs_1) < 0) {
-            return -1;
-        }
-
-        // the second layer
-        int sector_2 = addrs_1[addr_offset_1];
-        int addr_offset_2 = blockNum_in_double % addr_num;
-        uint16_t addrs_2[addr_num];
-        
-        if (diskimg_readsector(fd, sector_2, addrs_2) < 0) {
-            return -1;
-        }
-        return addrs_2[addr_offset_2];
-    }
+		// the second layer
+		int sector_2 = addrs_1[addr_offset_1];
+		int addr_offset_2 = blockNum_in_double % addr_num;
+		uint16_t addrs_2[addr_num];
+		int err_2 = diskimg_readsector(fd, sector_2, addrs_2);
+		if(err_2 < 0) return -1;
+		return addrs_2[addr_offset_2];
+	}	
 }
 
 int inode_getsize(struct inode *inp) {
