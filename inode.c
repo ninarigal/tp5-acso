fd71087d5d9af7182073561c7dc57023da0e2f1e
Nina Rigal <ninarigal@MacBook-Pro-de-Nina.local>
2024-06-04 17:58:18 -0300

    inode

--- inode.c
+++ inode.c
@@ -50,16 +50,81 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
 /**
  * TODO
  */
-int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {  
-    //Implement code here
-    // archivo distribuido en distintos bloques 
-    // blockNum es el indice del numero de bloque que quiero leer
-    // inp es el inodo que quiero leer -> dentro de la estructura inode tiene un atrubuto imode que me dice si es large file
-    // fs es el sistema de archivos
-    // quiero leer el bloque blockNum del inodo inp
-    // si es un archivo chico -> los primeros 7 bloques estan en el inodo
-    // si es un archivo grande -> los primeros 7 bloques estan en el inodo y el octavo esta en un bloque que esta en el inodo
+// int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {  
+//     //Implement code here
+//     // archivo distribuido en distintos bloques 
+//     // blockNum es el indice del numero de bloque que quiero leer
+//     // inp es el inodo que quiero leer -> dentro de la estructura inode tiene un atrubuto imode que me dice si es large file
+//     // fs es el sistema de archivos
+//     // quiero leer el bloque blockNum del inodo inp
+//     // si es un archivo chico -> los primeros 7 bloques estan en el inodo
+//     // si es un archivo grande -> los primeros 7 bloques estan en el inodo y el octavo esta en un bloque que esta en el inodo
 
+//     int small_file = ((inp->i_mode & ILARG) == 0);
+//     if (small_file) {            
+//         if (blockNum < 7) {
+//             return inp->i_addr[blockNum];
+//         } else {
+//             return -1;
+//         }
+//     } else {
+//         // si esta en los primeros 7 bloques (solo un indireccionamiento)
+//         int blockIndex = blockNum / 256;
+//         if (blockIndex <= 7) {
+//             void *buff= malloc(DISKIMG_SECTOR_SIZE);
+//             if (buff == NULL) {
+//                 return -1;
+//             }
+//             if (diskimg_readsector(fs->dfd, blockIndex, buff) == -1) {
+//                 return -1;
+//             }
+//             // ahora iteramos sobre el buffer cada 2 bytes buscando blocknum
+//             int *block = (int *)buff;
+//             int i;
+//             int blockIndex2 = blockNum - (256 * (blockIndex - 1));
+//             for (i = 0; i < 256; i++) {
+//                 int block = ((uint16_t *) buff)[i];
+//                 if (block == blockIndex2) {
+//                     free(buff);
+//                     return block;
+//                 }                   
+//             }
+//             free(buff);
+//             return -1;
+//         } else {
+//             // si esta en el octavo bloque (doble indireccionamiento)
+//             void *buff= malloc(DISKIMG_SECTOR_SIZE);
+//             if (buff == NULL) {
+//                 return -1;
+//             }
+//             if (diskimg_readsector(fs->dfd, blockIndex, buff) == -1) {
+//                 return -1;
+//             }
+//             int *block = (int *)buff;
+//             int i;
+//             int blockIndex2 = blockNum - (256 * (blockIndex - 1));
+//             for (i = 0; i < 256; i++) {
+//                 int block = ((uint16_t *) buff)[i];
+//                 if (block == blockIndex2) {
+//                     if (diskimg_readsector(fs->dfd, blockIndex, buff) == -1) {
+//                         return -1;
+//                     }
+//                     for (i = 0; i < 256; i++) {
+//                         int block = ((uint16_t *) buff)[i];
+//                         if (block == blockNum) {
+//                             free(buff);
+//                             return block;
+//                         }
+//                     }
+//                 }                   
+//             }
+//             free(buff);
+//             return -1;              
+//         }
+//     }
+// }
+
+int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {  
     int small_file = ((inp->i_mode & ILARG) == 0);
     if (small_file) {            
         if (blockNum < 7) {
@@ -68,59 +133,26 @@ int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum
             return -1;
         }
     } else {
-        int blockIndex = blockNum / 256;
-        if (blockIndex <= 7) {
-            void *buff= malloc(DISKIMG_SECTOR_SIZE);
-            if (buff == NULL) {
-                return -1;
-            }
-            if (diskimg_readsector(fs->dfd, blockIndex, buff) == -1) {
-                return -1;
-            }
-            // ahora iteramos sobre el buffer cada 2 bytes buscando blocknum
-            int *block = (int *)buff;
-            int i;
-            int blockIndex2 = blockNum - (256 * (blockIndex - 1));
-            for (i = 0; i < 256; i++) {
-                int block = ((uint16_t *) buff)[i];
-                if (block == blockIndex2) {
-                    free(buff);
-                    return block;
-                }                   
-            }
-            free(buff);
-            return -1;
+        if (blockNum < 7) {
+            return inp->i_addr[blockNum];
         } else {
-            void *buff= malloc(DISKIMG_SECTOR_SIZE);
+            int indirect_block_index = inp->i_addr[7];  // El octavo bloque es el puntero indirecto
+            void *buff = malloc(DISKIMG_SECTOR_SIZE);
             if (buff == NULL) {
                 return -1;
             }
-            if (diskimg_readsector(fs->dfd, blockIndex, buff) == -1) {
+            if (diskimg_readsector(fs->dfd, indirect_block_index, buff) == -1) {
+                free(buff);
                 return -1;
             }
-            int *block = (int *)buff;
-            int i;
-            int blockIndex2 = blockNum - (256 * (blockIndex - 1));
-            for (i = 0; i < 256; i++) {
-                int block = ((uint16_t *) buff)[i];
-                if (block == blockIndex2) {
-                    if (diskimg_readsector(fs->dfd, blockIndex, buff) == -1) {
-                        return -1;
-                    }
-                    for (i = 0; i < 256; i++) {
-                        int block = ((uint16_t *) buff)[i];
-                        if (block == blockNum) {
-                            free(buff);
-                            return block;
-                        }
-                    }
-                }                   
-            }
+            int block_offset = blockNum - 7;
+            int block = ((int *)buff)[block_offset];
             free(buff);
-            return -1;              
+            return block;
         }
     }
 }
+
 
 int inode_getsize(struct inode *inp) {
   return ((inp->i_size0 << 16) | inp->i_size1);  // devuelve la cantidad de bytes que tiene el archivo 
