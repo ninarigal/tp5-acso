462190bc3fca90f7ddcba9d275f1e13594f650fc
Nina Rigal <ninarigal@MacBook-Pro-de-Nina.local>
2024-06-05 12:56:51 -0300

    inode

--- inode.c
+++ inode.c
@@ -62,46 +62,82 @@ int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum
     // si es un archivo chico -> los primeros 7 bloques estan en el inodo
     // si es un archivo grande -> los primeros 7 bloques estan en el inodo y el octavo esta en un bloque que esta en el inodo
 
-    int small_file = ((inp->i_mode & ILARG) == 0);
-    if (small_file) {
-        if (blockNum < 7) {
-            return inp->i_addr[blockNum];
-        } else {
-            return -1;
-        }
-    }
+    // int small_file = ((inp->i_mode & ILARG) == 0);
+    // if (small_file) {
+    //     if (blockNum < 7) {
+    //         return inp->i_addr[blockNum];
+    //     } else {
+    //         return -1;
+    //     }
+    // }
 
-    if (blockNum < (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)) * 7) { // 512 / 2 * 7 (entran 256 bloques por sector) -> esta en los primeros 7 (1er nivel de indireccionamiento)
-        int sectorNum = blockNum / (DISKIMG_SECTOR_SIZE / sizeof(uint16_t));     
-        void *buff= malloc(DISKIMG_SECTOR_SIZE);
-        if (buff == NULL) {
-            return -1;
-        }
-        if (diskimg_readsector(fs->dfd, inp->i_addr[sectorNum], buff) == -1) {
-            return -1;
-        }
-        uint16_t *block = (uint16_t *)buff;
-        int block_number = block[blockNum % (DISKIMG_SECTOR_SIZE / sizeof(uint16_t))];
-        free(buff);
-        return block_number;
+    // if (blockNum < (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)) * 7) { // 512 / 2 * 7 (entran 256 bloques por sector) -> esta en los primeros 7 (1er nivel de indireccionamiento)
+    //     int sectorNum = blockNum / (DISKIMG_SECTOR_SIZE / sizeof(uint16_t));     
+    //     void *buff= malloc(DISKIMG_SECTOR_SIZE);
+    //     if (buff == NULL) {
+    //         return -1;
+    //     }
+    //     if (diskimg_readsector(fs->dfd, inp->i_addr[sectorNum], buff) == -1) {
+    //         return -1;
+    //     }
+    //     uint16_t *block = (uint16_t *)buff;
+    //     int block_number = block[blockNum % (DISKIMG_SECTOR_SIZE / sizeof(uint16_t))];
+    //     free(buff);
+    //     return block_number;
    
-    } else { // sino hay dos niveles de indireccionamiento
-        int sectorNum = 7 + (blockNum - (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)) * 7) / (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)); // 7 + (bloque - 512) / 256
-        void *buff= malloc(DISKIMG_SECTOR_SIZE);
-        if (buff == NULL) {
-            return -1;
-        }
-        if (diskimg_readsector(fs->dfd, inp->i_addr[7], buff) == -1) {
-            return -1;
-        }
-        uint16_t *block = (uint16_t *)buff;
-        if (diskimg_readsector(fs->dfd, block[sectorNum], buff) == -1) {
-            return -1;
-        }
-        int block_number = block[(blockNum - (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)) * 7) % (DISKIMG_SECTOR_SIZE / sizeof(uint16_t))];
-        free(buff);
-        return block_number;
-    }
+    // } else { // sino hay dos niveles de indireccionamiento
+    //     int sectorNum = 7 + (blockNum - (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)) * 7) / (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)); // 7 + (bloque - 512) / 256
+    //     void *buff= malloc(DISKIMG_SECTOR_SIZE);
+    //     if (buff == NULL) {
+    //         return -1;
+    //     }
+    //     if (diskimg_readsector(fs->dfd, inp->i_addr[7], buff) == -1) {
+    //         return -1;
+    //     }
+    //     uint16_t *block = (uint16_t *)buff;
+    //     if (diskimg_readsector(fs->dfd, block[sectorNum], buff) == -1) {
+    //         return -1;
+    //     }
+    //     int block_number = block[(blockNum - (DISKIMG_SECTOR_SIZE / sizeof(uint16_t)) * 7) % (DISKIMG_SECTOR_SIZE / sizeof(uint16_t))];
+    //     free(buff);
+    //     return block_number;
+    // }
+
+    int fd = fs->dfd;
+	int is_small_file = ((inp->i_mode & ILARG) == 0);
+
+	// if it is a small file
+	if(is_small_file) {
+		return inp->i_addr[blockNum];
+	}	
+
+	// if it is a large file
+	int addr_num = DISKIMG_SECTOR_SIZE / sizeof(uint16_t);
+	int indir_addr_num = addr_num * 7;
+	if(blockNum < indir_addr_num) {		// if it only uses INDIR_ADDR
+		int sector_offset = blockNum / addr_num;
+		int addr_offset = blockNum % addr_num;
+		uint16_t addrs[addr_num];
+		int err = diskimg_readsector(fd, inp->i_addr[sector_offset], addrs);
+		if(err < 0) return -1;	
+		return addrs[addr_offset];
+	} else {							// if it also uses the DOUBLE_INDIR_ADDR
+		// the first layer
+		int blockNum_in_double = blockNum - indir_addr_num;
+		int sector_offset_1 = 7;
+		int addr_offset_1 = blockNum_in_double / addr_num;
+		uint16_t addrs_1[addr_num];
+		int err_1 = diskimg_readsector(fd, inp->i_addr[sector_offset_1], addrs_1);
+		if(err_1 < 0) return -1;
+
+		// the second layer
+		int sector_2 = addrs_1[addr_offset_1];
+		int addr_offset_2 = blockNum_in_double % addr_num;
+		uint16_t addrs_2[addr_num];
+		int err_2 = diskimg_readsector(fd, sector_2, addrs_2);
+		if(err_2 < 0) return -1;
+		return addrs_2[addr_offset_2];
+	}	
     
 }
 
