ead3f487476a109778de632eef822d312845265d
Nina Rigal <ninarigal@MacBook-Pro-de-Nina.local>
2024-06-06 17:16:02 -0300

    try

--- inode.c
+++ inode.c
@@ -42,44 +42,38 @@ int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum
     }
     // if not a small file, it is a large file
     int blocks_per_sector = DISKIMG_SECTOR_SIZE / sizeof(uint16_t); // 256
-    int first_indirect_limit = blocks_per_sector * 7; // 1792
-    if (blockNum < first_indirect_limit) { // if the blockNum is less than 1792, it is a first level of indirection
+    int first_indir_limit = blocks_per_sector * 7; // 1792
+    void *buffer_1 = malloc(DISKIMG_SECTOR_SIZE);
+    if (buffer_1 == NULL) {
+        return -1;
+    }
+    if (blockNum < first_indir_limit) { // if the blockNum is less than 1792, it is a first level of indirection
         int indir_index = blockNum / blocks_per_sector; // index of the indirect block
         int offset = blockNum % blocks_per_sector; // offset in the indirect block
-        void *buffer = malloc(DISKIMG_SECTOR_SIZE);
-        if (buffer == NULL) {
-            return -1; 
-        }
-        if (diskimg_readsector(fs->dfd, inp->i_addr[indir_index], buffer) == -1) {
-            free(buffer);
-            return -1; 
-        }
-        int block_number = ((uint16_t *)buffer)[offset];
-        free(buffer);
-        return block_number;
-    } else { // if the blockNum is greater than 1792, it is a second level of indirection
-        int second_level_start = first_indirect_limit; // 1792
-        int first_indir_index = 7; 
-        void *buffer_1 = malloc(DISKIMG_SECTOR_SIZE);
-        if (buffer_1 == NULL) {
-            return -1;  
-        }
-        if (diskimg_readsector(fs->dfd, inp->i_addr[first_indir_index], buffer_1) == -1) { // read the first level indirect block
+        if (diskimg_readsector(fs->dfd, inp->i_addr[indir_index], buffer_1) == -1) {
             free(buffer_1);
             return -1; 
         }
-        int second_indir_index = (blockNum - second_level_start) / blocks_per_sector; // index of the second level indirect block
+        int block_number = ((uint16_t *)buffer_1)[offset];
+        free(buffer_1);
+        return block_number;
+    } else { // if the blockNum is greater than 1792, it is a second level of indirection
+        if (diskimg_readsector(fs->dfd, inp->i_addr[7], buffer_1) == -1) { // read the 8th block (first level indirect block)
+            free(buffer_1);
+            return -1; 
+        }
         void *buffer_2 = malloc(DISKIMG_SECTOR_SIZE);
         if (buffer_2 == NULL) {
             free(buffer_1);
             return -1; 
         }
+        int second_indir_index = (blockNum - first_indir_limit) / blocks_per_sector; // index of the second level indirect block
         if (diskimg_readsector(fs->dfd, ((uint16_t *)buffer_1)[second_indir_index], buffer_2) == -1) { // read the second level indirect block
             free(buffer_1);
             free(buffer_2);
             return -1; 
         }
-        int offset = (blockNum - second_level_start) % blocks_per_sector; // offset in the second level indirect block
+        int offset = (blockNum - first_indir_limit) % blocks_per_sector; // offset in the second level indirect block
         int block_number = ((uint16_t *)buffer_2)[offset]; // get the block number
         free(buffer_1);
         free(buffer_2);
