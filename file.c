d3bdb1cba3760127dd2f19190979f44507b5ac49
Nina Rigal <ninarigal@MacBook-Pro-de-Nina.local>
2024-06-06 10:05:00 -0300

    try

--- file.c
+++ file.c
@@ -43,26 +43,55 @@ int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *bu
     //     return total_size % DISKIMG_SECTOR_SIZE;
     // }
     // get inode content
-	struct inode my_inode;
-	int err = inode_iget(fs, inumber, &my_inode);
-	if(err < 0) return -1;
+	// struct inode my_inode;
+	// int err = inode_iget(fs, inumber, &my_inode);
+	// if(err < 0) return -1;
 
-	// get true block num
-	int sector = inode_indexlookup(fs, &my_inode, blockNum);
-	if(sector < 0) return -1;
+	// // get true block num
+	// int sector = inode_indexlookup(fs, &my_inode, blockNum);
+	// if(sector < 0) return -1;
 
-	// get block content
-	int read_err = diskimg_readsector(fs->dfd, sector, buf);
-	if(read_err < 0) return -1;
+	// // get block content
+	// int read_err = diskimg_readsector(fs->dfd, sector, buf);
+	// if(read_err < 0) return -1;
 
-	// get bytes and blocks
-	int total_bytes = inode_getsize(&my_inode);
-	if(total_bytes < 0) return -1;
-	int total_blocks = total_bytes / DISKIMG_SECTOR_SIZE;
-	if(blockNum == total_blocks) {
-		return total_bytes % DISKIMG_SECTOR_SIZE;
-	} else {
-		return DISKIMG_SECTOR_SIZE;
-	}
+	// // get bytes and blocks
+	// int total_bytes = inode_getsize(&my_inode);
+	// if(total_bytes < 0) return -1;
+	// int total_blocks = total_bytes / DISKIMG_SECTOR_SIZE;
+	// if(blockNum == total_blocks) {
+	// 	return total_bytes % DISKIMG_SECTOR_SIZE;
+	// } else {
+	// 	return DISKIMG_SECTOR_SIZE;
+	// }
+
+    struct inode *inp = malloc(sizeof(struct inode));
+    if (inp == NULL) {
+        return -1;
+    }
+    if (inode_iget(fs, inumber, inp) == -1) {
+        free(inp);
+        return -1;
+    }
+    int sector = inode_indexlookup(fs, inp, blockNum); 
+    if (sector == -1) {
+        free(inp);
+        return -1;
+    }
+    if (diskimg_readsector(fs->dfd, sector, buf) == -1) {
+        free(inp);
+        return -1;
+    }
+    int total_size = inode_getsize(inp);
+    if (total_size <= 0) {
+        free(inp);
+        return -1;
+    }
+    int blocks = total_size / DISKIMG_SECTOR_SIZE;
+    if (blockNum == blocks) {
+        return total_size % DISKIMG_SECTOR_SIZE;
+    } else {
+        return DISKIMG_SECTOR_SIZE;
+    }
 }
 
